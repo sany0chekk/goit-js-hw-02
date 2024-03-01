591fce07ecf2855a5dba25859461dca9314fc881
Olexandr Veselyi <olexandr.veseluy@gmail.com>
2024-03-01 13:07:57 +0200

    v0.0.1

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function checkForSpam(message) {
+  const lowerCase = message.toLowerCase();
+  const result =
+    lowerCase.includes('spam') || lowerCase.includes('sale') ? true : false;
+
+  return result;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
